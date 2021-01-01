09a4a12445b1b8a2f7e024e06dc0813911479ce3
kurnet <shumball@hotmail.com>
2021-01-02 03:11:00 +0800

    show gps

--- script.js
+++ script.js
@@ -93,3 +93,20 @@ function renderPlaces(places) {
         scene.appendChild(model);
     });
 }
+
+AFRAME.registerComponent('rotation-reader', {
+  tick: function () {
+    // `this.el` is the element.
+    // `object3D` is the three.js object.
+
+    // `rotation` is a three.js Euler using radians. `quaternion` also available.
+    console.log(this.el.object3D.rotation);
+
+    // `position` is a three.js Vector3.
+    console.log(this.el.object3D.position);
+	
+	const div = document.querySelector('.gpsinfo');
+    div.innerText = this.el.object3D.position;
+	
+  }
+});
