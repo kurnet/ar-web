7a04ccab90ab079b4c54ca5c7bda58c9153319e0
kurnet <shumball@hotmail.com>
2021-02-05 19:46:48 +0800

    test spark

---
+++ func/inc.php
@@ -0,0 +1,6 @@
+<?
+header("Content-Type:text/html; charset=utf-8");
+$dbconn = mysqli_connect("127.0.0.1", "kurnet_jobadmin", "KNet1212", "kurnet_ar_hunt", 3306);
+mysqli_set_charset ($dbconn,"utf8");
+
+?>
